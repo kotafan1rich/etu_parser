13201a5c8d75e252d1cca645b7a98a8748a27eef
Aleksis <lesapond2007@gmail.com>
2025-07-15 01:27:34 +0300

    add poly parser

--- bot/src/checker/utils.py
+++ bot/src/checker/utils.py
@@ -1,35 +1,35 @@
 import asyncio
 import logging
-from urllib.parse import urlparse, parse_qs
-from aiohttp import ClientSession
-from bs4 import BeautifulSoup
+from urllib.parse import parse_qs, urlparse
 
+from aiohttp import ClientSession, ClientTimeout
+from bs4 import BeautifulSoup
 from src.checker.router import active_users
 from src.create_bot import bot
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-HEADERS = {
-    "Accept": "text/html",
-    "Accept-Language": "ru,en;q=0.9",
-    "Connection": "keep-alive",
-    "If-None-Match": '"63a9d2087705d1c89dae206245b142c9"',
-    "Origin": "https://abit.etu.ru",
-    "Referer": "https://abit.etu.ru/",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-site",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
-            (KHTML, like Gecko) Chrome/134.0.0.0 YaBrowser/25.4.0.0 Safari/537.36",
-    "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "YaBrowser";v="25.4", \
-            "Yowser";v="2.5"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-}
-
 
 class EtuParser:
+    HEADERS = {
+        "Accept": "text/html",
+        "Accept-Language": "ru,en;q=0.9",
+        "Connection": "keep-alive",
+        "If-None-Match": '"63a9d2087705d1c89dae206245b142c9"',
+        "Origin": "https://abit.etu.ru",
+        "Referer": "https://abit.etu.ru/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+                (KHTML, like Gecko) Chrome/134.0.0.0 YaBrowser/25.4.0.0 Safari/537.36",
+        "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "YaBrowser";v="25.4", \
+                "Yowser";v="2.5"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+    }
+
     PROGRAMS = {
         "Прикладная математика и информатика": {
             "code": "01.03.02",
@@ -158,7 +158,8 @@ class EtuParser:
         },
     }
 
-    def __init__(self, session: ClientSession):
+    def __init__(self, user_id_in_etu: str, session: ClientSession):
+        self.user_id_in_etu = user_id_in_etu
         self.session = session
 
     async def program_for_user(self, url_id: str, users: dict) -> list:
@@ -167,7 +168,9 @@ class EtuParser:
         params = {
             "id": url_id,
         }
-        async with self.session.get(url=url, params=params, timeout=5) as response:
+        async with self.session.get(
+            url=url, params=params, headers=EtuParser.HEADERS
+        ) as response:
             if response.status != 200:
                 return None
             html = await response.text()
@@ -188,12 +191,14 @@ class EtuParser:
                     break
             return users
 
-    async def get_programs_ids(self) -> list | None:
+    async def get_programs_ids(self):
         url = "https://lists.priem.etu.ru/public/page"
         params = {
             "id": "01978f0d-1f93-7ee6-8067-bee68ca59d4f",
         }
-        async with self.session.get(url=url, params=params, timeout=5) as response:
+        async with self.session.get(
+            url=url, params=params, headers=EtuParser.HEADERS
+        ) as response:
             if response.status != 200:
                 return None
 
@@ -218,22 +223,21 @@ class EtuParser:
             "bodyOnly": "true",
         }
 
-        async with self.session.get(url=url, params=params, timeout=5) as response:
+        async with self.session.get(
+            url=url, params=params, headers=EtuParser.HEADERS
+        ) as response:
             if response.status != 200:
                 return None
 
             html = await response.text()
             soup = BeautifulSoup(html, "lxml")
             res = {}
-            my_pos = 0
             for row in soup.find_all("tr"):
                 rows_data = [i.text.strip() for i in row.find_all("td")]
                 if rows_data:
                     id = rows_data[1]
                     quata = rows_data[3]
-                    if id == "3675991" or quata not in ("БВИ", "Основные места"):
-                        my_pos = int(rows_data[0])
-                        continue
+
 
                     rate = int(rows_data[4])
                     priority = int(rows_data[2])
@@ -242,10 +246,11 @@ class EtuParser:
                         break
 
                     res[id] = {"rate": rate, "priority": priority}
-            return res, my_pos
+            return res
 
-    async def get_current_pos(self) -> tuple[int | None, int | None]:
-        users_upper, my_pos = await self.get_main_table()
+    async def get_current_pos(self, programm_name) -> tuple[int | None, int | None]:
+        users_upper = await self.get_main_table()
+        my_pos = len(users_upper) + 1
         logger.info(f"Users upper: {len(users_upper)}, my_pos: {my_pos}")
 
         if users_upper and my_pos:
@@ -254,20 +259,173 @@ class EtuParser:
 
             for name in self.PROGRAMS:
                 program_id = self.PROGRAMS[name]["id"]
-                if program_id != "01978f0d-1f93-7ee6-8067-bee68ca59d4f":
+                if name != programm_name:
                     users_upper = await self.program_for_user(program_id, users_upper)
-                    logger.info(f"{program_id} parsed")
-
+                logger.info(f"Program Etu {name} parsed")
             current_pos = len(users_upper) + 1
         return current_pos, my_pos
 
 
-async def sender():
+class PolyParser:
+    HEADERS = {
+        "accept": "*/*",
+        "accept-language": "ru,en;q=0.9",
+        "content-type": "application/json",
+        "origin": "https://my.spbstu.ru",
+        "priority": "u=1, i",
+        "sec-ch-ua": '"Chromium";v="136", "YaBrowser";v="25.6", "Not.A/Brand";v="99", "Yowser";v="2.5"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 YaBrowser/25.6.0.0 Safari/537.36",
+        "x-csrftoken": "undefined",
+    }
+
+    def __init__(self, session, user_id):
+        self.session = session
+        self.user_id = user_id
+
+    async def get_programs_id(self) -> list[str]:
+        json_data = {
+            "id_1": "2",
+            "id_2": "1",
+            "education_level": "bachelor",
+        }
+        async with self.session.post(
+            "https://my.spbstu.ru/home/get-code-list",
+            headers=self.HEADERS,
+            json=json_data,
+        ) as response:
+            if response.status != 200:
+                response.raise_for_status()
+            json_data = await response.json()
+            codes = json_data.get("code_list", [])
+            valid_ids = [
+                code.get("id")
+                for code in codes
+                if "ИНО" not in code.get("title", False)
+            ]
+            return valid_ids
+
+    async def get_target_program_table(self, program_id: str) -> dict:
+        params = {
+            "filter_1": "2",
+            "filter_2": "1",
+            "filter_3": program_id,
+            "education_level": "bachelor",
+        }
+        async with self.session.get(
+            "https://my.spbstu.ru/home/get-abit-list",
+            params=params,
+            headers=self.HEADERS,
+        ) as response:
+            if response.status != 200:
+                response.raise_for_status()
+            json_data = await response.json()
+            results = json_data.get("results", {})
+            table = {}
+            k = 1
+            for i in results:
+                if i["code"] not in table:
+                    table[i["code"]] = i
+                    i["num"] = k
+                    k += 1
+            return table
+
+    async def get_places(self, program_id: str) -> int:
+        json_data = {
+            "id_3": program_id,
+            "education_level": "bachelor",
+        }
+        async with self.session.post(
+            "https://my.spbstu.ru/home/get-direction-info",
+            headers=self.HEADERS,
+            json=json_data,
+        ) as response:
+            if response.status != 200:
+                response.raise_for_status()
+            json_data = await response.json()
+            return int(json_data[0].get("places", 0))
+
+    def get_concurents(self, table: dict, my_pos: int) -> dict:
+        concurrents = {}
+        for key, value in table.items():
+            if value["num"] < my_pos:
+                concurrents[key] = value
+        return concurrents
+
+    def clear_concurents(self, concurrents: dict, table, places: int) -> dict:
+        for code, value in concurrents.copy().items():
+            if code in table:
+                cur_pr = concurrents[code]["priority"]
+                prog_pr = table[code]["priority"]
+                prog_pos = table[code]["num"]
+                if prog_pos <= places and cur_pr > prog_pr:
+                    concurrents.pop(code)
+        return concurrents
+
+
+async def get_my_poly_pos(session, user_id: str) -> tuple[int, int, int]:
+    select_program_id = "847"  # Программная инженерия
+    logger.info("Start Poly Parser")
+    parser = PolyParser(session, user_id)
+    ids = await parser.get_programs_id()
+    target_program_table = await parser.get_target_program_table(select_program_id)
+    places_target = await parser.get_places(select_program_id)
+    my_pos = target_program_table.get(user_id).get("num")
+    concurents = parser.get_concurents(target_program_table, my_pos)
+    logger.info(
+        f"Concurents count: {len(concurents)}, my_pos: {my_pos}, places: {places_target}"
+    )
+    for program_id in ids:
+        if program_id == select_program_id:
+            continue
+        program_table = await parser.get_target_program_table(program_id)
+        places = await parser.get_places(program_id)
+        concurents = parser.clear_concurents(
+            concurrents=concurents, table=program_table, places=places
+        )
+        logger.info(f"Poly Program {program_id} parsed")
+    return my_pos, len(concurents), places_target
+
+
+async def get_my_etu_pos(
+    session: ClientSession,
+    user_id_in_etu: str,
+    programm_name: str = "Программная инженерия",
+) -> tuple[int | None, int | None, int]:
+    logger.info("Start Etu Parser")
+    parser = EtuParser(user_id_in_etu=user_id_in_etu, session=session)
+    pos, current_pos = await parser.get_current_pos(programm_name=programm_name)
+    general_budget_seats = EtuParser.PROGRAMS[programm_name]["general_budget_seats"]
+    return pos, current_pos, general_budget_seats
+
+
+async def sender(
+    programm_name: str = "Программная инженерия", user_id: str = "3675991"
+):
     while True:
-        async with ClientSession(headers=HEADERS) as session:
-            parser = EtuParser(session=session)
-            pos, current_pos = await parser.get_current_pos()
-            mes = f"Ваша позиция: {pos}({current_pos}) / 44"
+        async with ClientSession(timeout=ClientTimeout()) as session:
+            etu_task = asyncio.create_task(
+                get_my_etu_pos(
+                    session=session,
+                    user_id_in_etu=user_id,
+                    programm_name=programm_name,
+                )
+            )
+            poly_task = asyncio.create_task(
+                get_my_poly_pos(session=session, user_id=user_id)
+            )
+            etu_pos, etu_concurents, etu_places = await etu_task
+            poly_pos, poly_concurents, poly_places = await poly_task
+            mes = "\n".join(
+                (
+                    f"Позиция в ЛЭТИ: {etu_concurents + 1} ({etu_pos}) / {etu_places} мест",
+                    f"Позиция в Политехе: {poly_concurents + 1} ({poly_pos}) / {poly_places} мест",
+                )
+            )
 
             for user_id in active_users:
                 await bot.send_message(user_id, mes)
